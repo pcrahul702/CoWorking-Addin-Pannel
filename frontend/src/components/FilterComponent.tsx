1cfbe9021c1b4a67d24cad04acfed5b9b10d2554
rahul <pcrahul702@gmail.com>
2024-08-18 20:55:28 +0530

    location filter added

--- frontend/src/components/FilterComponent.tsx
+++ frontend/src/components/FilterComponent.tsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dropdown from './DropDown';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from "../redux/store";
+import { setFilteredProperties } from "../redux/slices/PropertiesSlice";
+import { PropertywithId } from '../Interface/interface';
 
 interface FilterOption {
   label: string;
@@ -20,12 +24,34 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
   const [price, setPrice] = useState('');
   const [location, setLocation] = useState('');
   const [propertyType, setPropertyType] = useState('');
+  const dispatch = useDispatch();
+  const properties = useSelector(
+    (state: RootState) => state.PropertiesSlice.value
+  );
 
   const resetFilters = () => {
     setPrice('');
     setLocation('');
     setPropertyType('');
   };
+
+  useEffect(() => {
+    const handleFilter = () => {
+      if (price || propertyType || location) {
+        const data = properties.filter((item: PropertywithId) => {
+          return (
+            item.location.toLowerCase().includes(location.toLowerCase()) 
+            
+          );
+        });
+        dispatch(setFilteredProperties(data));
+      } else {
+        dispatch(setFilteredProperties([]));
+      }
+    };
+
+    handleFilter();
+  }, [price, propertyType, location, dispatch, properties]);
 
   return (
     <div className="flex justify-between items-center text-white">
