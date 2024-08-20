f5f2c5773aa8bea6f078f1cb645b1ad645fc5d42
rahul kumar <pcrahul702@gmail.com>
2024-08-20 17:39:15 +0530

    working on filter

--- backend/src/Routes/properties.ts
+++ backend/src/Routes/properties.ts
@@ -1,11 +1,46 @@
-import express from "express";
+import express, { response } from "express";
 import Property from "../Models/properties";
 const router = express.Router();
-router.get("/", async (req, res) => {
- 
+router.post("/", async (req, res) => {
+  const { filter } = req.body;
+  console.log(filter)
   try {
+    // const isFilterEmpty = !filter || Object.keys(filter).length === 0;
+    // let query: { [key: string]: any } = {};
+
+    // if (!isFilterEmpty) {
+    //   // Build the query object if filter is not empty
+    //   if (filter.price) {
+    //     if (filter.price === 'low') {
+    //       query.price = { $lte: 1000 };
+    //     } else if (filter.price === 'medium') {
+    //       query.price = { $gt: 1000, $lte: 4000 };
+    //     } else if (filter.price === 'large') {
+    //       query.price = { $gt: 4000 };
+    //     }
+    //   }
+
+    //   if (filter.propertyType) {
+    //     query.propertyType = filter.propertyType;
+    //   }
+
+    //   if (filter.location) {
+    //     query.location = filter.location;
+    //   }
+    // }
+
     const properties = await Property.find();
-    res.json(properties);
+  
+    let FilteredProperties:any
+    if(filter.location){
+      FilteredProperties =properties.filter((item)=>{
+        console.log(item.location.includes(filter.location))
+      })
+    }
+    console.log(FilteredProperties)
+   let resp=(FilteredProperties && FilteredProperties.length>0)?FilteredProperties:properties;
+   console.log(resp)
+    
   } catch (err) {
     console.log(err)
     res.status(500).json({  message: "something Went Wrong" });
